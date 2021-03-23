16e0c2cf8ffe09c4f1af6086a856cfbebd1f7a78
paytonealexis <paytonealexis@gmail.com>
2021-03-22 19:12:47 -0700

    Create s3.tf

---
+++ terraform/aws/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials"
+    Environment = local.resource_prefix.value
+  }
+
+}
