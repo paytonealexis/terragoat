423a56ef5f1d4b83db7aea5c5506d7dca2154269
paytonealexis <paytonealexis@gmail.com>
2021-03-22 19:10:07 -0700

    Create s3.tf

---
+++ terraform/aws/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "data" {
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
